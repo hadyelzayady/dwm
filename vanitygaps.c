cb029f19e1098b40dc80c2c0aa7c0bbaff03c10c
HadyElzayady <hadyelzayady@yahoo.com>
2020-12-15 15:49:42 +0200

    cfact patch to resize windows in stack file

--- vanitygaps.c
+++ vanitygaps.c
@@ -504,16 +504,14 @@ static void
 tile(Monitor *m)
 {
 	unsigned int i, n;
+	int oh, ov, ih, iv;
 	int mx = 0, my = 0, mh = 0, mw = 0;
 	int sx = 0, sy = 0, sh = 0, sw = 0;
 	float mfacts, sfacts;
 	int mrest, srest;
 	Client *c;
 
-
-	int oh, ov, ih, iv;
 	getgaps(m, &oh, &ov, &ih, &iv, &n);
-
 	if (n == 0)
 		return;
 
@@ -525,7 +523,7 @@ tile(Monitor *m)
 
 	if (m->nmaster && n > m->nmaster) {
 		sw = (mw - iv) * (1 - m->mfact);
-		mw = (mw - iv) * m->mfact;
+		mw = mw - iv - sw;
 		sx = mx + mw + iv;
 	}
 
@@ -533,10 +531,10 @@ tile(Monitor *m)
 
 	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if (i < m->nmaster) {
-			resize(c, mx, my, mw - (2*c->bw), (mh / mfacts) + (i < mrest ? 1 : 0) - (2*c->bw), 0);
+			resize(c, mx, my, mw - (2*c->bw), mh * (c->cfact / mfacts) + (i < mrest ? 1 : 0) - (2*c->bw), 0);
 			my += HEIGHT(c) + ih;
 		} else {
-			resize(c, sx, sy, sw - (2*c->bw), (sh / sfacts) + ((i - m->nmaster) < srest ? 1 : 0) - (2*c->bw), 0);
+			resize(c, sx, sy, sw - (2*c->bw), sh * (c->cfact / sfacts) + ((i - m->nmaster) < srest ? 1 : 0) - (2*c->bw), 0);
 			sy += HEIGHT(c) + ih;
 		}
 }
