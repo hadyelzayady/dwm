f51e29dbc89355f333a3a4455330203109a9aed7
HadyElzayady <hadyelzayady@yahoo.com>
2021-01-05 12:40:48 +0200

    focus when click patch

--- config.h
+++ config.h
@@ -19,6 +19,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
